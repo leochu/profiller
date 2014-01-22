94e27c7be70f8d7e1438934c4d552baf5d0bc382
Leo Chu <leochu@Leo-Chus-iMac.local>
2014-01-22 02:45:48 -0800

    db scripts

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -8,7 +8,7 @@ create table base_model (
   constraint pk_base_model primary key (id))
 ;
 
-create table user (
+create table prof_user (
   id                        varchar(40) not null,
   email                     varchar(255),
   first_name                varchar(255),
@@ -16,8 +16,8 @@ create table user (
   secret                    varchar(512),
   salt                      varchar(512),
   email_md5                 varchar(512),
-  constraint uq_user_email unique (email),
-  constraint pk_user primary key (id))
+  constraint uq_prof_user_email unique (email),
+  constraint pk_prof_user primary key (id))
 ;
 
 
@@ -29,7 +29,7 @@ SET REFERENTIAL_INTEGRITY FALSE;
 
 drop table if exists base_model;
 
-drop table if exists user;
+drop table if exists prof_user;
 
 SET REFERENTIAL_INTEGRITY TRUE;
 
